660797926fc808cb4e96f8d21cd6330f5148360f
vvvader <112002355+vvvader@users.noreply.github.com>
2022-08-24 11:52:06 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,12 @@
+public class Hello {
+	public static void main(String[] args) {
+
+	System.out.println("<Your Name>");
+	System.out.println("<Java Program Name>");
+	System.out.println("<Program/Project Description>");
+	System.out.println("<Date Created>");
+	System.out.println("<Course Number and Section Number>");
+	System.out.println();
+}
+
+}
